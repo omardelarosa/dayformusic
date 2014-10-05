132da5d29dea8ce05d0dd4332f991a67806dadff
omardelarosa <thedelarosa@gmail.com>
2014-10-05 14:53:11 -0400

    organized server code a bit

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 var express = require('express')
   , logfmt = require('logfmt')
-  , bodyParser = require('body-parser');
+  , bodyParser = require('body-parser')
+  , mongo = require('mongoskin')
+  , moment = require('moment')
+  , path = require('path')
+  , db = mongo.db(process.env.MONGOLAB_URI, {native_parser:true});
 
-// middleware
-var app = express();
+module.exports.start = function(done){
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+  // middleware
+  var app = express();
 
-// parse application/json
-app.use(bodyParser.json())
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(express.static(__dirname + '/public'));
-app.use(logfmt.requestLogger());
+  // parse application/json
+  app.use(bodyParser.json())
 
-var port = Number(process.env.PORT || 5001);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+  app.use(express.static(__dirname + '/public'));
+  app.use(logfmt.requestLogger());
+
+  var port = Number(process.env.PORT || 5001);
+
+  var server = app.listen(port, function(){
+    console.log("Listening on port " + port);
+    
+    if (done) {
+      return done(null, app, server);
+    }
+  });
+
+  server.on('error', function(e){
+    if (e.code == 'EADDRINUSE') {
+      console.log("Address in use.  Is the server already running?");
+    }
+    if (done) {
+      return done(e);
+    }
+  });
+
+}
+
+
+// Run the server if someone runs this file directly
+if(path.basename(process.argv[1], '.js') === path.basename(__filename, '.js')) {
+  module.exports.start();
+}
