f74bb92ecade152a0394a69d64afbadd928bd6b9
omardelarosa <thedelarosa@gmail.com>
2015-01-17 13:24:51 -0500

    added d3 and c3 libraries and created basic splash chart

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,14 @@ var jQuery = $;
 var bodyTemplate = require('../templates/body');
 var Router = require("./router");
 var Backbone = require('backbone');
+var d3 = require('d3');
+var c3 = require('c3');
+
 Backbone.$ = $;
 
 $(function(){
   
+  console.log("C3", c3)
   new Router();
 
   Backbone.history.start()
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,7 +1,10 @@
 var $ = require("jquery")
   , Backbone = require('backbone')
   , Day = require('../collections/day')
-  , Handlebars = require('handlebars');
+  , Handlebars = require('handlebars')
+  , d3 = require('d3')
+  , c3 = require('c3');
+
 Backbone.$ = $;
 
 var HomeViews = {
@@ -86,6 +89,43 @@ var HomeViews = {
         scoreAverage: avg
       }));
       this.appendCovers();
+
+      var chart = c3.generate({
+            bindto: '.gauge-big',
+            data: {
+                columns: [
+                    ['average', Math.round(avg*100)/100]
+                ],
+                type: 'gauge',
+                onclick: function (d, i) { console.log("onclick", d, i); },
+                onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+                onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+            },
+            gauge: {
+              label: {
+                 format: function(value, ratio) {
+                     return value;
+                 },
+                 // show: false // to turn off the min/max labels.
+              },
+            min: 0, // 0 is default, //can handle negative min e.g. vacuum / voltage / current flow / rate of change
+            max: 10.0, // 100 is default
+            units: '',
+               width: 39 // for adjusting arc thickness
+            },
+            color: {
+                pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+                threshold: {
+                  unit: 'points', // percentage is default
+                  max: 10.0, // 100 is default
+                  values: [4.0, 6.0, 8.0, 9.0, 10.0]
+                }
+            },
+            size: {
+              height: 200
+            }
+        });
+
       return this;
     },
 
